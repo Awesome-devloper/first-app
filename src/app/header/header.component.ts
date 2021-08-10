2f68c99b75e95d69af032ae6f1f178ff993cf163
Awesome-devloper <aref.saas@gmail.com>
2021-08-10 20:20:00 +0430

    Add some functionality chapter 6

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,7 +1,18 @@
-import { Component } from '@angular/core';
+import { EventEmitter } from '@angular/core';
+import { Component, Output } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+ @Output('ChoiceManue') ChoiceManue = new EventEmitter<{ selected: string[] }>();
+
+ RecipesCalled(){
+   this.ChoiceManue.emit({selected:['Recipes']});
+  
+ }
+ ShoppingListCalled(){
+  this.ChoiceManue.emit({selected:['Shopping']});
+ }
+}
